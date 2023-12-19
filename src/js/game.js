f15e034b3978e6c720a90ab4a02d802ac9047fbe
Marohha <marohha@yandex.ru>
2023-12-19 22:04:11 +0300

    HW help

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+import character from './domain.js';
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+}  
+export default Game;
+  
+  export class GameSavingData {
+  }
+  
+  export function readGameSaving() {
+  }
+  
+  export function writeGameSaving() {
+  }
+
+
